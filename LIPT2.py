e948bbd3509f5508676093f771980d7a91cf03ae
veerjakhar <155930805+veerjakhar@users.noreply.github.com>
2025-12-05 17:19:47 -0800

    Add files via upload

--- LIPT2.py
+++ LIPT2.py
@@ -4,4 +4,11 @@ top = Tk()
 top.title("Login")
 top.config(background = "red")
 top.geometry("450x300")
+
+# Create A Label
+username = Label(top, text = "Username").place(x = 40, y = 60)
+password = Label(top, text = "Password").place(x = 40, y = 100)
+sub = Button(top, text = "Submit").place(x = 40, y = 140)
+usernameput = Entry(top, width = 30).place(x = 140, y = 60)
+passwordput = Entry(top, show = '*', width = 30).place(x = 140, y = 100)
 top.mainloop()
