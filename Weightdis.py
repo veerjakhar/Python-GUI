37141f6b4725270771aa366ced131e130bf1574b
veerjakhar <155930805+veerjakhar@users.noreply.github.com>
2026-01-07 19:02:17 -0800

    Add files via upload

---
+++ Weightdis.py
@@ -0,0 +1,55 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("900x200")
+root.title("Weight Distributer")
+
+def frompound():
+    pounds = float(lbsentry.get())
+
+    g = pounds * 453.592
+    kg = pounds * 0.453592
+    flz = pounds * 16
+    
+    gentery.delete("1.0", END)
+    gentery.insert(END, g)
+
+    kgentry.delete("1.0", END)
+    kgentry.insert(END, kg)
+
+    flzentry.delete("1.0", END)
+    flzentry.insert(END, flz)
+
+pounds = StringVar()
+
+lbs = Label(root, text = "Enter the weight in pounds")
+lbs.grid(row = 1, column = 1)
+
+lbsentry = Entry(root, width = 30)
+lbsentry.grid(row = 1, column = 2)
+
+conv = Button(root, text = "Convert", command = frompound)
+conv.grid(row = 1, column = 3)
+
+g = Label(root, text = "Gram(s)")
+g.grid(row = 2, column = 1)
+
+kg = Label(root, text = "Kilogram(s)")
+kg.grid(row = 2, column = 2)
+
+flz = Label(root, text = "Ounce(s)")
+flz.grid(row = 2, column = 3)
+
+gentery = Text(root, width = 30, height = 1)
+gentery.grid(row = 3, column = 1)
+
+lbsentry = Text(root, width = 30, height = 1)
+lbsentry.grid(row = 3, column = 2)
+
+flzentry = Text(root, width = 30, height = 1)
+flzentry.grid(row = 3, column = 3)
+
+
+
+
+root.mainloop()
