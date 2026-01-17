8737f1f1348430766cf09d29d8c47c9ef55ae9a8
veerjakhar <155930805+veerjakhar@users.noreply.github.com>
2026-01-16 17:46:39 -0800

    Add files via upload

--- RPS.py
+++ RPS.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 
 root = Tk()
 root.geometry("700x300")
@@ -12,34 +13,88 @@ def compwins():
     global playerscore, computerscore
     computerscore += 1
     wins.config(text = "!Computer Won!")
+    computersc.config(text = "Computer Score: " + str(computerscore))
+    playersc.config(text = "Your Score: " + str(playerscore))
+
+def playerwins():
+    global playerscore, computerscore
+    playerscore += 1
+    wins.config(text = "!Player Won!")
+    computersc.config(text = "Computer Score: " + str(computerscore))
+    playersc.config(text = "Your Score: " + str(playerscore))
+
+def tiechange():
+    global playerscore, computerscore
+    wins.config(text = "!It's a Tie!")
+    computersc.config(text = "Computer Score: " + str(computerscore))
+    playersc.config(text = "Your Score: " + str(playerscore))
+
+def playerchoice(player_input):
+    global playerscore, computerscore
+    print(player_input)
+    computer_input = get_computer_choice()
+    print(computer_input)
+    ursel.config(text = "You Seleceted: " + player_input[0])
+    compsl.config(text = "Computer Selected: " + computer_input[0])
+    if (player_input == computer_input):
+        tiechange()
+
+    # If the Player has Choosen Rock
+    if (player_input[1] == 0):
+        if (computer_input[1] == 1):
+            compwins()
+        elif (computer_input[1] == 2):
+            playerwins()
+    # If the Player has Choosen Paper
+    elif (player_input[1] == 1):
+        if (computer_input[1] == 0):
+            playerwins()
+        elif (computer_input[1] == 2):
+            compwins()
+    # If the Player has Choosen Scissors
+    elif (player_input[1] == 2):
+        if (computer_input[1] == 0):
+            compwins()
+        elif (computer_input[1] == 1):
+            playerwins()
+
+def get_computer_choice():
+    return random.choice(options)
 
 rpsl = Label(root, text = "Rock Paper Scissors")
-rpsl.grid(row = 1, column = 3)
+rpsl.pack()
 
 wins = Label(root, text = "Lets Start the Game", fg = "green")
-wins.grid(row = 2, column = 3)
+wins.pack()
 
-optl = Label(root, text = "Your Options:")
-optl.grid(row = 3, column = 1)
+inputf = Frame(root)
+inputf.pack()
 
-rockb = Button(root, text = "Rock")
-rockb.grid(row = 3, column = 2)
+optl = Label(inputf, text = "Your Options:")
+optl.grid(row = 0, column = 0, pady = 8)
 
-paperb = Button(root, text = "Paper")
-paperb.grid(row = 3, column = 3)
+rockb = Button(inputf, text = "Rock", width = 15, bd = 0, pady = 5, command = lambda: playerchoice(options[0]))
+rockb.grid(row = 1, column = 1, padx = 8, pady = 5)
 
-scisb = Button(root, text = "Sissors")
-scisb.grid(row = 3, column = 4)
+paperb = Button(inputf, text = "Paper", width = 15, bd = 0, pady = 5, command = lambda: playerchoice(options[1]))
+paperb.grid(row = 1, column = 2, padx = 8, pady = 5)
 
-scrl = Label(root, text = "Score")
-scrl.grid(row = 4, column = 1)
+scisb = Button(inputf, text = "Scissors", width = 15, bd = 0, pady = 5, command = lambda: playerchoice(options[2]))
+scisb.grid(row = 1, column = 3, padx = 8, pady = 5)
 
-url = Label(root, text = "Your Score:")
-url.grid(row = 5, column = 2)
+scrl = Label(inputf, text = "Score:")
+scrl.grid(row = 2, column = 0)
 
-compl = Label(root, text = "Computer Score:")
-compl.grid(row = 6, column = 2)
+ursel = Label(inputf, text = "You Selected:")
+ursel.grid(row = 3, column = 1)
 
+compsl = Label(inputf, text = "Computer Selected:")
+compsl.grid(row = 4, column = 1)
 
+playersc = Label(inputf, text = "Player Score:")
+playersc.grid(row = 3, column = 2)
+
+computersc = Label(inputf, text = "Computer Score:")
+computersc.grid(row = 4, column = 2)
 
 root.mainloop()
