7a96a971220f9f653e42133924e62ee2273271c2
veerjakhar <155930805+veerjakhar@users.noreply.github.com>
2026-01-30 17:56:55 -0800

    Add files via upload

--- timer.py
+++ timer.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter.ttk import *
-from time import strftime
+import time
+from tkinter import messagebox
 
 root = Tk()
 root.title("Timer")
@@ -14,16 +15,38 @@ hours.set("00")
 minutes.set("00")
 seconds.set("00")
 
-hours = Entry(root, width = 3, font = ("Arial", 18, ""), textvariable = hours)
-hours.place(x = 100, y = 60)
+def submit():
+    try:
+        temp = int(hours.get()) * 3600 + int(minutes.get()) * 60 + int(seconds.get())
+    except ValueError:
+        print("Please input the right value")
+        return
+    while temp >= 0:
+        mins, secs = divmod(temp, 60)
+        hurs = 00
+        if mins > 59:
+            hurs, mins = divmod(mins, 60)
+            hours.set("{:02d}".format(hurs))
+            minutes.set("{:02d}".format(mins))
+            seconds.set("{:02d}".format(secs))
+            root.update()
+            time.sleep(1)
+        if temp == 0:
+            messagebox.showinfo('showinfo', "Time Countdown, Times Up")
+            break
+        temp -= 1
 
-mins = Entry(root, width = 3, font = ("Arial", 18, ""), textvariable = minutes)
-mins.place(x = 150, y = 60)
 
-secs = Entry(root, width = 3, font = ("Arial", 18, ""), textvariable = seconds)
-secs.place(x = 200, y = 60)
+hourse = Entry(root, width = 3, font = ("Arial", 18, ""), textvariable = hours)
+hourse.place(x = 100, y = 60)
 
-timers = Button(root, text = "Start Timer")
+minse = Entry(root, width = 3, font = ("Arial", 18, ""), textvariable = minutes)
+minse.place(x = 150, y = 60)
+
+secse = Entry(root, width = 3, font = ("Arial", 18, ""), textvariable = seconds)
+secse.place(x = 200, y = 60)
+
+timers = Button(root, text = "Start Timer", command = submit)
 timers.place(x = 120, y = 100)
 
 root.mainloop()
